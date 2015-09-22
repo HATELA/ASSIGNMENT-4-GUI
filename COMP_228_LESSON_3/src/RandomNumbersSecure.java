b938efefa5ec333f4902d9fbfadcd6d5c9d92160
HATELA <patelshivam3033@gmail.com>
2015-09-22 14:29:13 -0700

    Random numbers of dice

--- COMP_228_LESSON_3/src/RandomNumbersSecure.java
+++ COMP_228_LESSON_3/src/RandomNumbersSecure.java
@@ -5,18 +5,18 @@ public class RandomNumbersSecure {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Dint die1 = 0;//
-		//Dint die2 = 0;//
-		//Dint dice = 0;//
+		//Dint die1 = 0;
+		//Dint die2 = 0;
+		//Dint dice = 0;
 		
 		
 		
 		//dice = die1 + die2;//
 		
-		System.out.println(rolldice(2,6));
+		System.out.println(rolldice(3,6));
 		
 		
-		
+		//If the numDice = 2 then there are in total 12 numbers. therefore result = genRand.nextInt(12) + 1;
 
 	}
 	public static int rolldice(int numDice, int numSides )
@@ -27,7 +27,7 @@ public class RandomNumbersSecure {
 		
 		for(int index=0; index < numDice-1; index++)
 		{
-			result = generateRandom.nextInt(6) + 1;
+			result = generateRandom.nextInt(18) + 1;
 		}
 		return result;
 	}
