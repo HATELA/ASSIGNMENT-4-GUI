a7c0383a139e4d08031ac14cb7e9771dfb637a69
HATELA <patelshivam3033@gmail.com>
2015-10-06 07:46:01 -0700

    INITIAL COMMIT

---
+++ COMP_228_LESSON5_INHERITANCE/src/Program.java
@@ -0,0 +1,10 @@
+//DRIVER CLASS+++++++++++++++++++++++++++++++++++++++++++++
+public class Program {
+
+	//MAIN METHOD+++++++++++++++++++++++++++++++++++++++++++
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
